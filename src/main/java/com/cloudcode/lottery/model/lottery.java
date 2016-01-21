c124d0d126c3553487535ea3b54168454bca0ff2
Administrator <Administrator@USER-20150923VB>
2016-01-21 12:44:59 +0800

    Signed-off-by: Administrator <Administrator@USER-20150923VB>

---
+++ src/main/java/com/cloudcode/lottery/model/lottery.java
@@ -0,0 +1,11 @@
+package com.cloudcode.lottery.model;
+
+public class lottery {
+	private int a;
+	private int b;
+	private int c;
+	private int d;
+	private int e;
+	private int f;
+	private int g;
+}
