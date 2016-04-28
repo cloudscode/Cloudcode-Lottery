d93293abea7e4e1a0476ed3715b6d362b37e49e5
Administrator <Administrator@USER-20150923VB>
2016-04-28 21:35:07 +0800

    Signed-off-by: Administrator <Administrator@USER-20150923VB>

---
+++ src/main/java/com/cloudcode/lottery/util/ForecastRunnable.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name:Cloudcode-Lottery
+ * File Name:ForecastUtil.java
+ * Package Name:com.cloudcode.lottery.util
+ * Date:2016-4-28下午5:06:49
+ * Copyright (c) 2016, chenzhou1025@126.com All Rights Reserved.
+ *
+*/
+
+package com.cloudcode.lottery.util;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ForecastRunnable implements Runnable{
+
+	public void run() {
+		
+	}
+
+}
+
